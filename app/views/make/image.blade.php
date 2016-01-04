c0c700a0fa69ee177d16691755c1dea9147db8e0
mingshi <fivemingshi@gmail.com>
2016-01-04 20:52:48 +0800

    .

--- app/views/make/image.blade.php
+++ app/views/make/image.blade.php
@@ -7,6 +7,8 @@
     <a href="javascript:;" class="file">
         <input type="file" name="" id="upfile" />选择文件
     </a>
+    <ul class="image-ul">
+    </ul>
     <a href="javascript:;" class="file next">
         <span>下一步</span>    
     </a>
@@ -14,8 +16,10 @@
 @stop
 
 @section('js')
+<!--
 <script type="text/javascript" src="/js/alloyimage.js"></script>
 <script type="text/javascript" src="/js/alloy.js"></script>
+-->
 <script src="/js/hammer.min.js"></script>
 <script src='/js/ImageEditor.js'></script>
 <script type="text/javascript">
@@ -53,9 +57,60 @@ $(function() {
                     editor.selectImage(0); // 初始化完成后，默认选中第一个图片（头像图片）作为当前编辑图片
                 }
             });
-            $('#index').val(0);
-        } 
+            var html = '<li><img id="thumb" style="position: relative; width:60px; height: 60px;" src="' + res + '" /><img src="/images/delete.png" style="position: absolute; margin-left:-15px; margin-top:-7px; width: 30px;" id="delete" /></li>';
+            $('ul.image-ul').append(html);
+            set_id();
+        }
     });
+
+    function set_id()
+    {
+        var len = editor.images.length;
+        var liLen = $('ul.image-ul li').length;
+        var start = parseInt(len) - 2;
+        for (var i=0; i < liLen; i++) {
+            $('ul.image-ul').children().eq(i).attr('id', start);
+            start--;
+        }
+        set_active(0);
+        each_click();
+        each_del();
+        return false;
+    }
+
+    function set_active(id)
+    {
+        $('ul.image-ul li').each(function(){
+            $(this).removeClass('active');
+        });
+    
+        $('ul.image-ul').children("li[id='"+ id + "']").addClass('active');
+    }
+
+    function each_click()
+    {
+        $('img#thumb').each(function() {
+            $(this).click(function() {
+                var id = $(this).parent().attr('id');
+                editor.selectImage(id);
+                set_active(id);
+                return false;
+            });
+        });
+    }
+
+    function each_del()
+    {
+        $('img#delete').each(function() {
+            $(this).unbind('click');
+            $(this).click(function() {
+                var id = $(this).parent().attr('id');
+                editor.removeImage(id);
+                $(this).parent().remove();
+                return false;
+            });
+        });
+    }
 });
 </script>
 @stop
